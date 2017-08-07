44350d8144afc9c22f109f14788805fad568898b
liuben10 <liuben10@gmail.com>
2017-08-06 23:41:34 -0700

    More practice

--- src/main/Fib.java
+++ src/main/Fib.java
@@ -6,7 +6,7 @@ package main;
 public class Fib {
 	public static void main(String...args) {
 		for (int i = 0; i < 10; i++) {
-			System.out.println(fib(i));
+			System.out.println(fastfib(i));
 		}
 	}
 
@@ -16,5 +16,24 @@ public class Fib {
 		} else {
 			return fib(n-1) + fib(n-2);
 		}
+	}
+
+
+	public static int fastfib(final int n) {
+		if (n <= 1) {
+			return 1;
+		}
+		int prev;
+		int cur = 1;
+		int sum = 2;
+		for(int i = 0; i < n; i++) {
+			if (i > 1) {
+				int tmp = cur;
+				cur = sum;
+				prev = tmp;
+				sum = cur + prev;
+			}
+		}
+		return sum;
 	}
 }
