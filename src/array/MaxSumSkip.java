e48d7a36e209f0fd52a35adc56097b0b5a160e04
liuben10 <liuben10@gmail.com>
2017-08-26 16:35:15 -0700

    Fix max sum skip problem

--- src/array/MaxSumSkip.java
+++ src/array/MaxSumSkip.java
@@ -12,22 +12,33 @@ public class MaxSumSkip {
 		int i = 0;
 		List<Integer> sumList = new ArrayList<>();
 		while (i < nums.length) {
-			System.out.println(i);
 			if (nums[i] > 0) {
 				sumList.add(nums[i]);
 				i += 1;
-			} else if (i + 1 < nums.length) {
-				int max = Math.max(nums[i], nums[i + 1]);
-				sumList.add(max);
-				if (max == nums[i + 1]) {
-					i += 2;
-				} else {
-					i += 1;
-				}
 			} else {
-				i += 1;
+				int j = i;
+				int oddSum = 0;
+				int evenSum = 0;
+				List<Integer> odds = new ArrayList<>();
+				List<Integer> evens = new ArrayList<>();
+				while (j < nums.length && nums[j] < 0) {
+					if ((j - i) % 2 == 0) {
+						evenSum += nums[j];
+						evens.add(nums[j]);
+					} else {
+						oddSum += nums[j];
+						odds.add(nums[j]);
+					}
+					j++;
+				}
+				int max = Integer.max(evenSum, oddSum);
+				if (max == evenSum) {
+					sumList.addAll(evens);
+				} else {
+					sumList.addAll(odds);
+				}
+				i = j;
 			}
-
 		}
 		return sumList;
 	}
@@ -35,6 +46,5 @@ public class MaxSumSkip {
 	public static void main(String...args) {
 		System.out.println(maxSum(new int[]{10, 20, 30, -10, -50, 40, -50, -1, -3}));
 		System.out.println(maxSum(new int[]{-1, -2, -3, -4, -5}));
-
 	}
 }
